85e124cfb7449981b10f50c9bd1c4a4526a1618e
Philip-Larsen-Donnelly <phil@dhis2.org>
2023-01-26 00:32:35 +0100

    feat: tag selectors and custom mkdocs-material

--- scripts/setup-deps.sh
+++ scripts/setup-deps.sh
@@ -5,3 +5,4 @@ pip3 install -r requirements.txt
 pip3 install -e tools/python/markdown-pp-master
 pip3 install -e tools/python/markdown-extensions/d_card
 pip3 install -e tools/python/dhis2_plugins
+pip3 install https://github.com/Philip-Larsen-Donnelly/mkdocs-material/archive/refs/heads/dhis2_custom.zip
