7117fccd8772066a351294b93857e78203c74f20
Philip-Larsen-Donnelly <phil@dhis2.org>
2024-07-13 01:25:37 +0200

    feat: add Kapa AI widget

---
+++ theme/resources/javascript/init_kapa_widget.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var script = document.createElement("script");
+    script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+    script.setAttribute("data-website-id", "27e730a7-0494-4dd9-8c0f-2536f5367212");
+    script.setAttribute("data-project-name", "DHIS2");
+    script.setAttribute("data-project-color", "#393D4D");
+    script.setAttribute("data-user-analytics-fingerprint-enabled", "true");
+    script.setAttribute("data-modal-disclaimer", "Answers are AI generated based on the DHIS2 Documentation. Be aware it might not be 100% accurate or up-to-date, check the sources to know for sure.");
+    script.setAttribute("data-project-logo", "/images/dhis2-icon-rgb-positive.png");
+    script.async = true;
+    document.head.appendChild(script);
+  });
