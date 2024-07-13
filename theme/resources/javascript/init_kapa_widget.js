baf6b322f21711ce37d3a3a56e97c16edb5cc1af
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2024-07-13 09:34:27 +0200

    tweak position of button above footer

--- theme/resources/javascript/init_kapa_widget.js
+++ theme/resources/javascript/init_kapa_widget.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("data-user-analytics-fingerprint-enabled", "true");
     script.setAttribute("data-modal-disclaimer", "Answers are AI generated based on the DHIS2 Documentation. Be aware it might not be 100% accurate or up-to-date, check the sources to know for sure.");
     script.setAttribute("data-project-logo", "/images/dhis2-icon-rgb-positive.png");
+    script.setAttribute("data-button-position-bottom", "60px");
     script.async = true;
     document.head.appendChild(script);
   });
