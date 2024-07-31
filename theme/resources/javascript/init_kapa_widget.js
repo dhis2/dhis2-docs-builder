bb09cec0423fc4d33b238ec657b582cb223f3b43
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2024-07-31 13:11:22 +0200

    Update init_kapa_widget.js

--- theme/resources/javascript/init_kapa_widget.js
+++ theme/resources/javascript/init_kapa_widget.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("data-project-name", "DHIS2");
     script.setAttribute("data-project-color", "#393D4D");
     script.setAttribute("data-user-analytics-fingerprint-enabled", "true");
-    script.setAttribute("data-modal-disclaimer", "Answers are AI generated based on the DHIS2 Documentation. Be aware it might not be 100% accurate or up-to-date, check the sources to know for sure.");
+    script.setAttribute("data-modal-disclaimer", "Answers are AI generated based on the DHIS2 Documentation. Be aware it might not be 100% accurate or up-to-date, check the sources to know for sure. Please note that all conversations are recorded by kapa.ai for quality and training purposes.");
     script.setAttribute("data-project-logo", "/images/dhis2-icon-rgb-positive.png");
     script.setAttribute("data-button-position-bottom", "60px");
     script.async = true;
